7b9255c16daf4c3647834ebbedc56f6c43b83e51
Dmitry Dolgashov <ddd2283@gmail.com>
2021-06-19 22:16:26 +0300

    add static pages + readme + initializer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.time_zone = 'Moscow'
+  I18n.config.available_locales = %i[ru]
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,9 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.time_zone = 'Moscow'
+  I18n.config.available_locales = %i[ru]
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
