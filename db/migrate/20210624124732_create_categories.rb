a111ed2f3ddabb9a2024e618f91a94282d1e5654
Dmitry Dolgashov <ddd2283@gmail.com>
2021-06-24 17:26:19 +0300

    Ochepyatki

--- db/migrate/20210624124732_create_categories.rb
+++ db/migrate/20210624124732_create_categories.rb
@@ -1,4 +1,4 @@
-class CreateCaategories < ActiveRecord::Migration[6.0]
+class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
       t.string :title, null: false, limit: 30
