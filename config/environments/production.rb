b825a7226e43bcfa73fd1c13c04b33f95160f127
Dmitry Dolgashov <ddd2283@gmail.com>
2021-09-03 14:57:01 +0300

    JS fixed!!!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,12 @@
 Rails.application.configure do
+
+  config.action_controller.asset_host = 'https://vast-sea-10906.herokuapp.com/'
+  config.action_mailer.asset_host = 'https://vast-sea-10906.herokuapp.com/'
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'https://vast-sea-10906.herokuapp.com/' }
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
