fdb8479637f6ee1416f919b9fd5d4a5f926d6153
Dmitry Dolgashov <ddd2283@gmail.com>
2021-09-06 12:25:57 +0300

    JS fixed!!!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,21 +10,17 @@ class ApplicationController < ActionController::Base
     I18n.locale == I18n.default_locale ? {} : {lang: I18n.locale}
   end
 
-  def default_url_options
-    { lang: I18n.locale }
-  end
-
   private
 
   def set_locale
     I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(user)
     if current_user.is_admin?
       admin_tests_path
-    else current_user.is_admin?
-      root_path
+    else
+      super
     end
   end
 
