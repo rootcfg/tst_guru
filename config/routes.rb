b0cd99f1805dab3cab2206d98790c7c8f734725b
Dmitry Dolgashov <ddd2283@gmail.com>
2021-09-29 16:48:24 +0300

    Action mailer for form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'feedbak/index'
+  post 'feedbak/create'
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }, :controllers => {
     registrations: 'registrations'
   }
